51075afdaa960983a8a8775c180b32ab12a693f4
Mark Feltner <feltner.mj@gmail.com>
2012-12-04 18:25:58 -0600

    gunicorn deployment!

--- swatidj.py
+++ swatidj.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from flask import Flask
-from gevent import monkey; monkey.patch_all()
+#from gevent import monkey; monkey.patch_all()
 from apps.configure import init_app
 
 
