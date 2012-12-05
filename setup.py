7277c288bea2a0d1d75fd12285ea94092695143a
Mark Feltner <feltner.mj@gmail.com>
2012-12-04 18:10:14 -0600

    setupfile updated

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
-from disutils.core import setup
+from distutils.core import setup
 
 setup(
-    name='SwatiDJ',
+    name='swatidj',
     version='0.1dev',
-    packages=,
-    long_description=open('README.txt').read()
+    author='Mark Feltner',
+    packages=['swatidj'],
+    scripts=['app.py'],
+    license='LICENSE.txt',
+    description="Saraswati's DJ",
+    requires=[
+      "Fabric",
+      "Flask",
+      "gunicorn",
+      "Logbook",
+      "Flask-Cache",
+      "python-memcached",
+      "git+git://github.com/feltnerm/python-mpd2.git",
+    ],
+
 )
