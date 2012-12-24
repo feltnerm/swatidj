7570bf186c20eab5464e0af4701732616ee593ae
Mark Feltner <feltner.mj@gmail.com>
2012-12-24 00:09:17 -0600

    moved or removed

--- apps/extensions.py
+++
@@ -1,6 +0,0 @@
-from flask.ext.cache import Cache
-from lib.mpd_kit import MPDKit
-
-cache = Cache()
-mpd_kit = MPDKit()
-
--- apps/mpd_api/views.py
+++
@@ -1,9 +0,0 @@
-from flask import jsonify, request, abort
-from flask.views import MethodView
-
-from apps.extensions import mpd_kit
-
-"""
-    Should represent RESTful-esque ways to talk with MPD (specifically the mpd_kit)
-"""
-
